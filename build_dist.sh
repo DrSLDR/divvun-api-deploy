cf145d265acb69009c588adc7c2fb044a84f5f8a
Dylan Hand <dylan.hand@gmail.com>
2023-04-19 11:59:02 +0200

    Add quotes to env vars in build script

--- build_dist.sh
+++ build_dist.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 ROOT=$PWD
 DIST=$ROOT/dist
-rm -rf $DIST && mkdir $DIST
+rm -rf "$DIST" && mkdir "$DIST"
 
 echo "building msoffice addin"
 git clone --single-branch --depth=1 \
@@ -13,19 +13,19 @@ git clone --single-branch --depth=1 \
 cd divvun-gramcheck-web/msoffice
 npm ci
 npm run build
-mkdir $DIST/msoffice
-cp -R dist/* $DIST/msoffice/
+mkdir "$DIST"/msoffice
+cp -R dist/* "$DIST"/msoffice/
 
-cd $ROOT
+cd "$ROOT"
 
 echo "copying configs"
-cp docker-compose.yml $DIST
+cp docker-compose.yml "$DIST"
 
 git clone --single-branch --depth=1 \
     https://github.com/divvun/divvun-api.git || \
     (cd divvun-api && git pull && cd ..)
 sudo docker build --no-cache -t divvun/divvun-api divvun-api
-sudo docker save divvun/divvun-api | gzip > $DIST/divvun-api.tar.gz
+sudo docker save divvun/divvun-api | gzip > "$DIST"/divvun-api.tar.gz
 
-pushd $DIST
+pushd "$DIST"
 tar cvvfz "$ROOT/dist.tar.gz" .
